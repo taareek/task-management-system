5415bcd1f12283709388fa994a66475478e41b05
Tarek Aziz <tarekiub17@gmail.com>
2023-05-15 13:58:09 +0600

    navbar designed

--- task-management-frontend/frontend/src/app/navbar/navbar.component.ts
+++ task-management-frontend/frontend/src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
 
 @Component({
   selector: 'app-navbar',
